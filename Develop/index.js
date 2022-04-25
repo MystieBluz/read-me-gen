48e7f6b3fe25ba56b1b3ae646d9fb4cf4dae0fb1
Misty Duhart <mystie.smith@gmail.com>
2022-04-24 23:57:56 -0400

    updated index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,66 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'title',
+      },
+
+      {
+        type: 'input',
+        message: 'What is the description of the project?',
+        name: 'description',
+      },
+
+      {
+        type: 'input',
+        message: 'What are the installation instructions for the project?',
+        name: 'installation',
+      },
+
+      {
+        type: 'input',
+        message: 'How is your project used?',
+        name: 'usage',
+      },
+
+      {
+        type: 'input',
+        message: 'What are your project contribution guidelines?',
+        name: 'contribution',
+      },
+
+      {
+        type: 'input',
+        message: 'What are the testing instructions for the project?',
+        name: 'test',
+      },
+
+      {
+        type: 'list',
+        message: 'What licenses are need for this project?',
+        choice: ['MIT', 'ISC']
+      },
+
+      {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+      },
+
+      {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+      },
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
